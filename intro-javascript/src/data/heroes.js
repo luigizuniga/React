91b348b733e46c90e3a564baff54f730c7f0e9ef
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-11 21:08:48 -0300

    REACT - Importaciones por defecto

--- intro-javascript/src/data/heroes.js
+++ intro-javascript/src/data/heroes.js
@@ -2,8 +2,7 @@ export const heroes = [{
         id: 1,
         name: 'Batman',
         owner: 'DC'
-    },
-    {
+    }, +{
         id: 2,
         name: 'Spiderman',
         owner: 'Marvel'
---
+++ intro-javascript/src/data/manga.js
@@ -0,0 +1,27 @@
+const manga = [{
+        id: 1,
+        name: 'Bersek',
+        owner: 'Panini'
+    }, {
+        id: 2,
+        name: 'Naruto',
+        owner: 'Shonen'
+    },
+    {
+        id: 3,
+        name: 'Dragon Ball',
+        owner: 'Shonen'
+    },
+    {
+        id: 4,
+        name: 'Shingeki',
+        owner: 'Madhouse'
+    }
+];
+
+const platafom = ['PS4', 'XBOX ONE', 'PS4'];
+export {
+    manga as
+    default,
+    platafom
+}
