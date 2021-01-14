0f433197d0e0fa612bde1cad21afebea39426d43
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-14 00:21:42 -0300

    React App 01 - PropTypes required y Default Prop

--- counter-app/src/PrimeraApp.js
+++ counter-app/src/PrimeraApp.js
@@ -1,23 +1,40 @@
 
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
 /**
     Un patrón común en React es que un componente devuelva múltiples elementos.
     Los Fragmentos te permiten agrupar una lista de hijos sin agregar nodos extra al DOM
 */
 
-const PrimeraApp = () => {
-    const message = 'Bienvenido';
+const PrimeraApp = ({ title, subtitle, counter }) => {
+    const message = "Welcome";
     const user = {
         name: 'Luigi',
         lastname: 'Zuniga'
+    };
+
+    if(!counter) {
+        console.log("No se encuentra la propiedad counter : "  + counter);
     }
+
     return (
         <Fragment>
             <h1>{ message }</h1>
+            <p>{ subtitle }</p>
             <pre>{  JSON.stringify(user, null, 3) }</pre>
 
         </Fragment>
     );
+}
+
+PrimeraApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    counter: PropTypes.number.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitle: 'This a Functional Component'
 }
 
 export default PrimeraApp;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -4,7 +4,6 @@ import PrimeraApp from './PrimeraApp';
 
 import './index.css';
 
-
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp/>, divRoot);
+ReactDOM.render(<PrimeraApp title counter="init"/>, divRoot);
