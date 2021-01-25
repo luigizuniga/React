d9184c5757dd48fd12b59b8ad2b9d2480ad46c2a
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-25 18:41:51 -0300

    React App 03 - GIFExpertApp Creacion de lista de categorias

--- gif-expert-app/src/GifExpertApp.js
+++ gif-expert-app/src/GifExpertApp.js
@@ -1,10 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { useState,Fragment } from 'react';
 
 export const GifExpertApp = () => {
+   //const categories = ['Mary','Sebastian','Constanza'];
+
+   const [ categories, setCategories] = useState(['Mary','Sebastian','Constanza']);
+   const handleAdd = () => {
+    //    categories.push('Albert'); console.log(categories);
+        setCategories( cat => [...cat,'Luigi']);
+   }
+
     return (
         <Fragment>
             <h2>GitExpertApp</h2>
             <hr/>
+            <button onClick={ handleAdd }>Agregar</button>
+            <ol>
+                {
+                    categories.map(category =>  {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
         </Fragment>
     );
 }
