32543068689bc5231bab3160e3567dfcfeb8f139
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-25 13:58:39 -0300

    React App 02 - GIFExpertApp init y configuracion basica de componente GifExpertApp

---
+++ gif-expert-app/src/GifExpertApp.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+export const GifExpertApp = () => {
+    return (
+        <Fragment>
+            <h2>GitExpertApp</h2>
+            <hr/>
+        </Fragment>
+    );
+}
---
+++ gif-expert-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GifExpertApp } from './GifExpertApp';
+
+import './index.css';
+
+const divElement = document.querySelector('#root');
+
+ReactDOM.render(<GifExpertApp />,  divElement );
