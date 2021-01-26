8707650ebc76519222346604f03caa65a73a2fa6
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-26 18:09:32 -0300

    React App 03 - GIFExpertApp Custom Hooks para cargar Data

--- gif-expert-app/src/components/GifGrid.js
+++ gif-expert-app/src/components/GifGrid.js
@@ -1,47 +1,13 @@
-import React , { useState, useEffect, Fragment }from 'react';
-import { GifGridItem } from './GifGridItem';
-import { getGifs } from '../helpers/getGifs';
+import React , { Fragment }from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
-    const [ images , setImages ] = useState([]);
-
-
-    useEffect(() => {
-        getGifs( category )
-          // .then( imgs => setImages(imgs)
-            .then( setImages );
-    },[ category ]);
-
+    const { loading }  = useFetchGifs();
 
     return (
         <Fragment>
-        <h3>{ category }</h3>
-        <div className="card-grid">
-            <ol>
-                {
-                //#region Formas de Envio de Objeto imagen
-                /* Objeto completo
-                 * ===============
-                 * images.map( img => (<li key={ img.id }> {img.title }</li>))
-
-                 * Objeto desestructurado
-                 * images.map( ({ id, title }) => (
-                 * <li key={ id }> { title }</li>
-                 * ))
-                 */
-
-                /* Envio de imagen a componente GifGridItem
-                 * images.map( img =>(
-                 *   <GifGridItem key={ img.id} img={ img }/>
-                 *))
-                 */
-                //#endregion
-                images.map( (img)=>(
-                    <GifGridItem key={ img.id } {...img}/>
-                ))
-               }
-            </ol>
-        </div>
+            <h3>{ category }</h3>
+            { loading ? 'Cargando...' : 'Data cargada' }
         </Fragment>
     )
 }
