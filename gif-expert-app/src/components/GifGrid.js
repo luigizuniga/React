ca9afcf73c4e29decf8815f607983df79fa99457
luigizuniga <luigialbertozuniga@gmail.com>
2021-01-26 13:56:34 -0300

    React App 03 - GIFExpertApp Uso de useEffect y comportamiento en un componente funcional

--- gif-expert-app/src/components/GifGrid.js
+++ gif-expert-app/src/components/GifGrid.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React , { useState , useEffect }from 'react';
 
 export const GifGrid = ({ category }) => {
+    const [ count , setCount] = useState(0);
+
+    // useEffect
+    // El Hook de efecto te permite llevar a cabo efectos secundarios en componentes funcionales:
+    // Solo queremos que se ejecute la instancia de  getGifs cuando el componente es renderizado por primera vez
+    useEffect(() => {
+        getGifs();
+        // ,[] => el segundo argumento es un arreglo de dependencia, con esto la peticion se ejecutara solo una vez
+    },[]);
+
     const getGifs = async () => {
         const url =  'https://api.giphy.com/v1/gifs/search?q=Rick and Morty&limit=10&api_key=F5lvX58U4STuXT8YOaCaabLVkjiBefJm';
         const resp = await fetch(url);
@@ -16,11 +26,13 @@ export const GifGrid = ({ category }) => {
         console.log(gifs);
     }
 
-    getGifs();
+
 
     return (
         <div>
             <h3>{ category }</h3>
+            <p>{ count }</p>
+            <button onClick={ ()=> setCount( count + 1 )}>+1</button>
         </div>
     )
 }
