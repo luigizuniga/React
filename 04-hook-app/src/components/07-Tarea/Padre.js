d0d2da0a3e0bc7032808748aa74d95039bcee008
luigizuniga <luigialbertozuniga@gmail.com>
2021-02-04 22:46:17 -0300

    React App 04 - Hook Tareas sin resolver

---
+++ 04-hook-app/src/components/07-Tarea/Padre.js
@@ -0,0 +1,29 @@
+import React , { Fragment, useState } from 'react';
+import { Hijo } from './Hijo';
+
+export const Padre = () => {
+    const nums = [ 2,4,6,8,10 ];
+    const [ value, setValue ] = useState(0);
+
+    const increment = ( num ) => {
+        setValue( value + num);
+    }
+
+    return (
+        <Fragment>
+            <h1>Componente Padre</h1>
+            <p> Total: { value }</p>
+
+            <hr/>
+
+            {
+                nums.map( n =>(
+                    <Hijo key={ n }
+                          numero={ n }
+                          increment={ increment }
+                        />
+                ))
+            }
+        </Fragment>
+    )
+}
--- 04-hook-app/src/index.js
+++ 04-hook-app/src/index.js
@@ -11,7 +11,8 @@ import ReactDOM from 'react-dom';
 // import { Layout } from './components/05-useLayoutEffect/Layout';
 // import { Memorize } from './components/06-memos/Memorize';
 // import { MemoHook } from './components/06-memos/MemoHook';
-import  { CallBackHooks } from './components/06-memos/CallBackHooks';
+// import  { CallBackHooks } from './components/06-memos/CallBackHooks';
+import { Padre } from './components/07-Tarea/Padre';
 
 const hooksapp = document.querySelector('#root');
-ReactDOM.render( <CallBackHooks /> , hooksapp);
+ReactDOM.render( <Padre /> , hooksapp);
