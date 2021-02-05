7d86b786a1fefa0848594ff072b13f9940b80342
luigizuniga <luigialbertozuniga@gmail.com>
2021-02-04 22:51:19 -0300

    React App 04 - Hook Tarea resuelta con useCallBack

--- 04-hook-app/src/components/07-Tarea/Padre.js
+++ 04-hook-app/src/components/07-Tarea/Padre.js
@@ -1,13 +1,17 @@
-import React , { Fragment, useState } from 'react';
+import React , { Fragment, useState, useCallback } from 'react';
 import { Hijo } from './Hijo';
 
 export const Padre = () => {
     const nums = [ 2,4,6,8,10 ];
     const [ value, setValue ] = useState(0);
 
-    const increment = ( num ) => {
-        setValue( value + num);
-    }
+    // const increment = ( num ) => {
+    //     setValue( value + num);
+    // }
+
+    const increment = useCallback( ( num ) => {
+        setValue( c => c + num );
+    },[ setValue ]);
 
     return (
         <Fragment>
