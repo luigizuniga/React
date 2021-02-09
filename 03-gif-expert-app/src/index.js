fea5d4771d469185472376ef0e1ff79660d72f4d
luigizuniga <luigialbertozuniga@gmail.com>
2021-02-09 17:25:45 -0300

    React Project 01 - Inicializacion de App Heroes

--- 03-gif-expert-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GifExpertApp } from './GifExpertApp';
-
-import './index.css';
-
-const divElement = document.querySelector('#root');
-
-ReactDOM.render(<GifExpertApp />,  divElement );
