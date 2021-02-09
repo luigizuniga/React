b823210fe98d704a6596c8ade5695dc4c87655de
luigizuniga <luigialbertozuniga@gmail.com>
2021-02-09 15:39:50 -0300

    React 04 - Hooks useContext Nav

--- 04-hook-app/src/components/09-useContext/NavBar.js
+++ 04-hook-app/src/components/09-useContext/NavBar.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">REACT</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <Link className="navbar-brand" to="/">REACT</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <Link className="nav-link active" to="/">Home</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/">Home</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/about">About</NavLink>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
+                <NavLink exact activeClassName="active" className="nav-link" to="/login">Login</NavLink>
               </li>
             </ul>
           </div>
