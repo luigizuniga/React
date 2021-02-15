a490a19339cc65a72f337f854eee2aa890cbb9fe
luigizuniga <luigialbertozuniga@gmail.com>
2021-02-15 12:44:22 -0300

    React Project 01 - HeroesApp Creacion de RoComponente DcScreen visualizacion heroes

--- heroes-app/src/routers/DashboardRoutes.js
+++ heroes-app/src/routers/DashboardRoutes.js
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import { MarvelScreen } from '../components/marvel/MarvelScreen';
 import { NavBar } from "../components/ui/NavBar";
+import { DcScreen } from "../components/dc/DcScreen";
 
 export const DashboardRoutes = () => {
   return (
@@ -11,14 +12,10 @@ export const DashboardRoutes = () => {
       <div className="container mt-2">
           <Switch>
               <Route exact path="/marvel" component={ MarvelScreen } />
-              {/* <Route exact path="/marvel" component={  } />
-              <Route exact path="/marvel" component={  } />
-              <Route exact path="/marvel" component={  } /> */}
+              <Route exact path="/dc" component={ DcScreen } />
               <Redirect to="/marvel" />
           </Switch>
       </div>
     </Fragment>
   );
 };
-
-
--- heroes-app/src/selectors/getHeroesByPublisher.js
+++ heroes-app/src/selectors/getHeroesByPublisher.js
@@ -4,7 +4,7 @@ import { heroes } from '../data/heroes';
 //#region Filtro por Editorial
 //#endregion
 export const getHeroesByPublisher = (publisher) => {
-    const validPublishers = ['DC comics', 'Marvel Comics'];
+    const validPublishers = ['DC Comics', 'Marvel Comics'];
 
     if (!validPublishers.includes(publisher)) {
         throw new Error(`Publisher "${ publisher }" no es correcto`);
