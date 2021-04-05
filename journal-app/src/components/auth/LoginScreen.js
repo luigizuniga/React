60d6e744e6090b952b11f855b0f76ddb8d04fb38
luigizuniga <luigialbertozuniga@gmail.com>
2021-04-05 14:14:57 -0400

    React Project 02 - JournelApp - Se agregan estilos en login y register components

--- journal-app/src/components/auth/LoginScreen.js
+++ journal-app/src/components/auth/LoginScreen.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 export const LoginScreen = () => {
     return (
         <div>
-            <h1>LoginScreen</h1>
+            <h3 className="auth__title">Login</h3>
             <form>
 
                 <input
                     type="text"
                     placeholder="Email"
                     name="email"
-                    className="auth_input"
+                    className="auth__input"
                     autoComplete="off"
                 />
 
@@ -31,13 +31,14 @@ export const LoginScreen = () => {
 
                 <div className="auth__social-networks">
                     <p>Login with social networks</p>
-
                     <div className="google-btn">
-                        <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button"/>
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign with google</b>
+                        </p>
                     </div>
-                    <p className="btn-text">
-                        <b>Sign with google</b>
-                    </p>
                 </div>
 
                 <Link
